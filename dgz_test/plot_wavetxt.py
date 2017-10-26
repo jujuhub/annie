8f23c44e4791402324f7fa262fc4d44105309c67
Julie He <juhe@ucdavis.edu>
2017-10-25 17:20:23 -0700

    Adjusted plot_wavetxt.py script

--- dgz_test/plot_wavetxt.py
+++ dgz_test/plot_wavetxt.py
@@ -1,4 +1,5 @@
-# PMT pulse distribution script for digitizer data
+# PMT pulse distribution script for digitizer data output
+# by: Julie He <juhe@ucdavis.edu>
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,7 +8,7 @@ import sys
 FILE = sys.argv[1]
 SAVEFILENAME = 'my_charges.txt'
 
-PEAK_VOLTAGE = 0.080 # V ; need to play around with
+PEAK_VOLTAGE = 8. # mV ; need to play around with
 IMPEDANCE = 50. # Ohms
 dT = 2E-9 # s ; sampling interval
 
@@ -51,47 +52,64 @@ def main():
     charge = []
     voltage = 0.0
 #    frameStart = 7 # skip header info
-#    frameCounter = 0
+    frameCounter = 0
 
     j = 7
-    while j != len(data): # while we are not at the end of file
+    while j < len(data): # while we are not at the end of file
         # convert ADC to volts and store in temp list
-        holderArray.append(float(data[j] - (2**14)*0.86)*1000./8192.) # 0.86 hard coded
+        holderArray.append((float(data[j]) - (2**14)*0.86)*1000./8192.) # 0.86 hard coded
         j += 1 # increment sample counter
-        
-        if (j-1)%(recordLength + 7) == 0: # if at end of frame
+
+#        if j == len(data)-1: # if at end of file
+#            holderArray.append((float(data[len(data)-1]) - (2**14)*0.86)*1000./8192.)
+#            j += 1
+
+        if (j)%(recordLength + 7) == 0: # if at end of frame
             voltage = sum(holderArray) # sum up area of the frame
 
             skip_frame = False # initialize skip_frame variable
-#            frameCounter += 1 # increment frame count
+            frameCounter += 1 # increment frame count
 
             # sum pedestal over first 5% of frame
             pedestal = 0
             DC = 0
-            for k in range(0, len(holderArray)/20):
+            for k in range(len(holderArray)/20):
+                # set relative DC level
+                DC = float(DC*k) + holderArray[k]/float(k + 1)
                 # check for early pulses
-                DC = float(DC*(k)) + holderArray[k]/float(k + 1)
                 if abs(float(holderArray[k+1]) - DC) > PEAK_VOLTAGE: 
-                    # skip entire frame
-                    skip_frame == True
+                    skip_frame == True # skip entire frame
                 else: # if no peaks, add to pedestal
-                    pedestal += float(holderArray[k])
+                    pedestal += holderArray[k]
             pedestal *= len(holderArray)/k # fix
+            print("k = " + str(k))
+            print("length of holderArray: " + str(len(holderArray)))
 
             if skip_frame == True:
                 print("Skipped frame: " + str(frameCounter))
             else:
                 charge.append(-(voltage - pedestal)*dT/IMPEDANCE)
 
-                # plot event
-#            plt.ylim((-30, 10)) # V
+            # plot event
+#            plt.ylim((-50, 30)) # mV
+#            plt.title('Event ' + str(frameCounter))
 #            plt.plot(holderArray)
 #            plt.show()
 
             # clear for next event
             holderArray = []
             voltage = 0.0
-            j+=7
+            j+=7 # skip header info
+            print("j = " + str(j))
 
+    # check num of events
+    if (frameCounter == len(data)/1037):
+        print("Accounted for all events")
+    else:
+        print("Did not account for all events: " + str(len(data)/1037. - frameCounter))
 
-np.savetxt(('{0}').format(SAVEFILENAME), charge)
+    np.savetxt(('{0}').format(SAVEFILENAME), charge)
+    return
+
+if __name__ == '__main__':
+    main()
